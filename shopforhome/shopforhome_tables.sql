d4e921ee073ebd7f387585d7b50a31c2b88a8ab3
glcapstone11 <glcapstone11@gmail.com>
2022-08-20 05:54:07 +0530

    commit

---
+++ shopforhome/shopforhome_tables.sql
@@ -0,0 +1,158 @@
+CREATE DATABASE shopforhome;
+USE shopforhome;
+
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.cart
+(
+    user_id bigint NOT NULL,
+    CONSTRAINT cart_pkey1 PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.discount
+(
+    id varchar(255) NOT NULL,
+    status bigint,
+    CONSTRAINT discount_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS shopforhome.product_category
+(
+    category_id integer NOT NULL AUTO_INCREMENT,
+    category_name varchar(255) ,
+    category_type integer,
+    create_time timestamp ,
+    update_time timestamp ,
+    CONSTRAINT product_category_pkey PRIMARY KEY (category_id),
+    CONSTRAINT uk_6kq6iveuim6wd90cxo5bksumw UNIQUE (category_type)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.product_info
+(
+    product_id varchar(255)  NOT NULL,
+    category_type integer DEFAULT 0,
+    create_time timestamp ,
+    product_description character varying(255) ,
+    product_icon character varying(255) ,
+    product_name character varying(255)  NOT NULL,
+    product_price numeric(19,2) NOT NULL,
+    product_status integer DEFAULT 0,
+    product_stock integer NOT NULL,
+    update_time timestamp ,
+    CONSTRAINT product_info_pkey PRIMARY KEY (product_id),
+    CONSTRAINT product_info_product_stock_check CHECK (product_stock >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS shopforhome.users
+(
+    id bigint NOT NULL,
+    active boolean NOT NULL,
+    address character varying(255) ,
+    email character varying(255) ,
+    name character varying(255) ,
+    password character varying(255) ,
+    phone character varying(255) ,
+    role character varying(255) ,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT uk_sx468g52bpetvlad2j9y0lptc UNIQUE (email)
+);
+
+ALTER TABLE `shopforhome`.`users` 
+CHANGE COLUMN `id` `id` BIGINT NOT NULL AUTO_INCREMENT ;
+
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.order_main
+(
+    order_id bigint NOT NULL,
+    buyer_address character varying(255) ,
+    buyer_email character varying(255) ,
+    buyer_name character varying(255) ,
+    buyer_phone character varying(255) ,
+    create_time timestamp,
+    order_amount numeric(19,2) NOT NULL,
+    order_status integer NOT NULL DEFAULT 0,
+    update_time timestamp,
+    CONSTRAINT order_main_pkey PRIMARY KEY (order_id)
+);
+
+ALTER TABLE `shopforhome`.`order_main` 
+CHANGE COLUMN `order_id` `order_id` BIGINT NOT NULL AUTO_INCREMENT ;
+
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.product_in_order
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    category_type integer NOT NULL,
+    count integer,
+    product_description character varying(255)  NOT NULL,
+    product_icon character varying(255) ,
+    product_id character varying(255) ,
+    product_name character varying(255) ,
+    product_price numeric(19,2) NOT NULL,
+    product_stock integer,
+    cart_user_id bigint,
+    order_id bigint,
+    CONSTRAINT product_in_order_pkey PRIMARY KEY (id),
+    CONSTRAINT fkt0sfj3ffasrift1c4lv3ra85e FOREIGN KEY (order_id)
+        REFERENCES shopforhome.order_main (order_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT product_cart_fkey FOREIGN KEY (cart_user_id)
+        REFERENCES shopforhome.cart (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        ,
+    CONSTRAINT product_in_order_count_check CHECK (count >= 1),
+    CONSTRAINT product_in_order_product_stock_check CHECK (product_stock >= 0)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS shopforhome.wishlist
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    created_date timestamp ,
+    user_id bigint,
+    product_id character varying(255),
+    CONSTRAINT wishlist_pkey PRIMARY KEY (id),
+    CONSTRAINT product_wish_fkey FOREIGN KEY (product_id)
+        REFERENCES shopforhome.product_info (product_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT user_wish_Fkey FOREIGN KEY (user_id)
+        REFERENCES shopforhome.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE `shopforhome`.`discount`
+ADD COLUMN user_email VARCHAR(255);
+
+ALTER TABLE `shopforhome`.`discount` 
+ADD INDEX `user_email_fkey_idx` (`user_email` ASC) VISIBLE;
+;
+ALTER TABLE `shopforhome`.`discount` 
+ADD CONSTRAINT `user_email_fkey`
+  FOREIGN KEY (`user_email`)
+  REFERENCES `shopforhome`.`users` (`email`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  
+  ALTER TABLE `shopforhome`.`discount` 
+DROP PRIMARY KEY;
+;
+
+ALTER TABLE `shopforhome`.`discount` 
+ADD COLUMN `coupon` VARCHAR(255) NULL AFTER `user_email`,
+CHANGE COLUMN `id` `id` BIGINT NOT NULL ,
+ADD PRIMARY KEY (`id`);
+;
+
+ALTER TABLE `shopforhome`.`discount` 
+CHANGE COLUMN `id` `id` BIGINT NOT NULL AUTO_INCREMENT ;
